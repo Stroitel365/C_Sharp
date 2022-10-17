826630c6815874fdd200d051370aab1476953d0c
Polina <philippova.polina@gmail.com>
2022-10-17 15:02:54 +0300

    Add Sem9 and SEM9_HomeWork folders

---
+++ SEM9/Program.cs
@@ -0,0 +1,61 @@
+﻿// Задать значение и вывести все натуральные числа до этого числа 
+
+/* Console.Write(" Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Recursia(n, 1)); */
+
+/// Натуральные числа от n до m
+/* 
+Console.Write(" Введите число: min ");
+int min = int.Parse(Console.ReadLine()!);
+
+Console.Write(" Введите число: max ");
+int max = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(Recursia(max, min));
+ */
+// Задача 67 Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр
+//453 -> 12
+//45   -> 9
+
+/* 
+Console.Write(" Введите число:  ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма цифр в цисле равна : {SummaChisel(number)}");
+ */
+
+// Задача 69 Напишите программу, которая на вход принимет два числа А и В и возводит число А
+// в целую степень В с помощью рекурсии
+
+Console.Write(" Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write(" Введите в какую степень возводить:  ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write($" Число {a} в степени {b} равно {APowerB(a,b)} ");
+
+// FUNCTIONS
+
+
+string Recursia(int n, int minValue)
+{
+    if (minValue == n)
+    {
+        return n.ToString();
+    }
+    return (minValue + " " + Recursia(n, minValue + 1));
+}
+
+int SummaChisel(int value)
+{
+    if (value == 0) return 0;
+    return (value % 10 + SummaChisel(value / 10));
+}
+
+int APowerB(int number, int power)
+{
+    if(power==0) return 1;
+    return (number*APowerB(number,power-1));
+}
+
+
---
+++ SEM9_HomeWork/Program.cs
@@ -0,0 +1,70 @@
+﻿
+/* Задача 64: Задайте значение N. 
+Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+Выполнить с помощью рекурсии.
+
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1" */
+/* 
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumpersFromNto1(n, 1));
+ */
+
+/* Задача 66: Задайте значения M и N. 
+Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+ */
+
+/*  
+Console.Write("Введите минимальное число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите максимальное число:  ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write($" Сумма чисел от {a} до {b} равно {SumFromAtoB(a,b)}");
+
+
+ */
+
+/* 
+Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 9
+m = 3, n = 2 -> A(m,n) = 29
+ */
+/* 
+Console.Write("Введите первый аргумент: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второй аргумент: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write($" Функция Аккермна от аргументов от {a} и {b} равна {AkkFunction(a,b)}");
+
+ */
+//FUNCTIONS
+
+string NumpersFromNto1(int n, int minValue)
+{
+    if (minValue == n)
+    {
+        return n.ToString();
+    }
+    return (NumpersFromNto1(n, minValue + 1) + " " + minValue);
+}
+
+int SumFromAtoB(int numberMIN, int numberMAX)
+{
+    if (numberMAX == numberMIN) return numberMIN;
+    return (numberMAX + SumFromAtoB(numberMIN, numberMAX - 1));
+}
+
+
+int AkkFunction(int firstArgumentM, int secondArgumentN)
+{
+    if (firstArgumentM == 0) return secondArgumentN + 1;
+    if (secondArgumentN == 0 && firstArgumentM>0 ) return AkkFunction(firstArgumentM - 1, 1);
+    return AkkFunction(firstArgumentM - 1, AkkFunction(firstArgumentM, secondArgumentN - 1));
+}
